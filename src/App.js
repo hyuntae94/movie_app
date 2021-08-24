26cacf84dc8e00431749c9884ce7db1e09f9a6de
hyuntae <believe4279@gmail.com>
2021-08-24 23:45:22 +0900

    start movieApp

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
-/* eslint-disable */
-
 import React from "react";
-import PropTypes from "prop-types"
 
+class App extends React.Component {
+    state = {
+        isLoading : true,
+    }
 
-Movie.propTypes = {
-  name : PropTypes.string.isRequired, //name 프로퍼티는 필수
-  url : PropTypes.string.isRequired,
-  rating : PropTypes.number // rating프로퍼티는 필수아님 대신 존재한다면 number타입이여야함
-}
-
-
-function Movie({name, url, rating}){
-  return  (
-    <div>
-      <h3 >{name}</h3>
-      <img src={url} alt={name}/>
-      <span>{rating}/5.0</span>
-    </div>
-  )
-}
-
-
-const FavFood = [
-  {
-    id :1,
-    name : "알라딘",
-    url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
-    rating : "4.8"
-  },
-  {
-    id : 2,
-    name : "알라딘",
-    url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
-    rating : 4.3
-  }
-]
-
-function renderMovie(list){
-  return <Movie key={list.id} name={list.name} url={list.url} rating ={list.rating}/>
-}
-
-function App(){
-  return (
-    <div> 
-      <h1>MovieList</h1>
-      { FavFood.map(renderMovie) }
-    </div>
-  )
+    componentDidMount(){
+      setInterval( () => {
+        this.setState( { isLoading : false})
+      }, 3000)
+    }
+    render(){
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-function main(){
-  return (
-    ReactDOM.render(
-      <App />,
-      document.querySelector('#root')
-    )
-  )
-}
-main();
-// setInterval(main,1000);
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
