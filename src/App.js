462a45f52c293a910626a88ef0f768e6ee94943c
hyuntae <believe4279@gmail.com>
2021-08-24 16:06:25 +0900

    프로퍼티 타입 검사

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React from "react";
+/* eslint-disable */
 
-/* eslint-disable-next-line */
+import React from "react";
+import PropTypes from "prop-types"
+
+
+Movie.propTypes = {
+  name : PropTypes.string.isRequired, //name 프로퍼티는 필수
+  url : PropTypes.string.isRequired,
+  rating : PropTypes.number // rating프로퍼티는 필수아님 대신 존재한다면 number타입이여야함
+}
+
+
 function Movie({name, url, rating}){
   return  (
     <div>
       <h3 >{name}</h3>
       <img src={url} alt={name}/>
-      <span>{rating}</span>
+      <span>{rating}/5.0</span>
     </div>
   )
 }
+
 
 const FavFood = [
   {
     id :1,
     name : "알라딘",
     url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
-    rating : "rating is : 5.0"
+    rating : "4.8"
   },
   {
     id : 2,
     name : "알라딘",
     url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
-    rating : "rating is : 4.5"
+    rating : 4.3
   }
 ]
 
