337d9abb1bd0403f4fc68e9f6fcc8b0844a48f5d
hyuntae <believe4279@gmail.com>
2021-08-24 15:37:36 +0900

    map을 사용하여 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return <h1>Hello React!!</h1>
+/* eslint-disable-next-line */
+function Movie({name, url, rating}){
+  return  (
+    <div>
+      <h3 >{name}</h3>
+      <img src={url} alt={name}/>
+      <span>{rating}</span>
+    </div>
+  )
+}
+
+const FavFood = [
+  {
+    id :1,
+    name : "알라딘",
+    url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
+    rating : "rating is : 5.0"
+  },
+  {
+    id : 2,
+    name : "알라딘",
+    url : "https://m.media-amazon.com/images/I/91WNnQZdybL._AC_SL1500_.jpg",
+    rating : "rating is : 4.5"
+  }
+]
+
+function renderMovie(list){
+  return <Movie key={list.id} name={list.name} url={list.url} rating ={list.rating}/>
+}
+
+function App(){
+  return (
+    <div> 
+      <h1>MovieList</h1>
+      { FavFood.map(renderMovie) }
+    </div>
+  )
 }
 
 export default App;
