166ee9bccf163f1d4e76a4572c1ab2b17dd38027
hyuntae <believe4279@gmail.com>
2021-08-25 14:36:45 +0900

    css추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css"
 
-function Movie({ id, year, title, summary, poster }){
+function Movie({year, title, summary, poster, genres }){
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__data">
+                <h2 className="movie__title">{title}</h2>
+                <h3 className="movie__year">{year}</h3>
+                <ul className="movie__genres">
+                    {genres.splice(0,3).map( (genre, idx) => <li key={idx} className="genres__genre">{genre}</li>)}
+                </ul>
+                <p className="movie_summary">{summary.slice(0,150)}...</p>
+               
+            </div>
         </div>
     )
 }
@@ -14,7 +24,8 @@ Movie.propTypes = {
     year : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+    poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
