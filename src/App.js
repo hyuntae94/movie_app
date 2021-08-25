b0483c2450f8ee1fa8e57e5ca6c8369abfe70e1f
hyuntae <believe4279@gmail.com>
2021-08-25 13:20:13 +0900

    add Movie Component && api를 사용하여 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
+/* eslint-disable */
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
     state = {
         isLoading : true,
+        movies : [],
+    }
+
+    getMovies = async() => {
+      const { 
+        data : {
+           data : { movies }
+            }
+          } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+      this.setState({ movies , isLoading : false})
     }
 
     componentDidMount(){
-      setInterval( () => {
-        this.setState( { isLoading : false})
-      }, 3000)
+      this.getMovies();
     }
+
+    componentWillUnmount(){  
+    }
+
     render(){
-        const { isLoading } = this.state;
-        return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+        const { isLoading, movies } = this.state;
+        return <div>{isLoading ? "Loading..." : movies.map(movie => {
+          return <Movie 
+            key={movie.id} 
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+          />
+        })}</div>
     }
 }
 
